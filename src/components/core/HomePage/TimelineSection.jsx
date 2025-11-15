e6dbb30e6adc092e49fe3a2a2555903343b71a0a
iammilindpatel <milindpatel13298@gmail.com>
2025-11-15 21:50:39 +0530

    timeline section updated

--- src/components/core/HomePage/TimelineSection.jsx
+++ src/components/core/HomePage/TimelineSection.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 import Logo1 from "../../../assets/TimeLineLogo/Logo1.svg"
 import Logo2 from "../../../assets/TimeLineLogo/Logo2.svg"
 import Logo3 from "../../../assets/TimeLineLogo/Logo3.svg"
@@ -54,7 +55,7 @@ const TimelineSection = () => {
                 } )
             }
         </div>
-        <div className='relative w-fit h-fit shadow-blue-200 shadow-[0px_0px_30px_0px]'>
+        <div className='relative w-fit h-fit shadow-blue-200 shadow-[0px_0px_30px_0px] '>
 
             <img  src={timelineImage}
             alt="timelineImage"
@@ -63,14 +64,14 @@ const TimelineSection = () => {
 
             <div className='absolute bg-caribbeangreen-700 flex flex-row text-white uppercase py-7
                             left-[50%] translate-x-[-50%] translate-y-[-50%]'>
-                <div className='flex flex-row gap-5 items-center border-r border-caribbeangreen-300 px-7'>
-                    <p className='text-3xl font-bold'>10</p>
-                    <p className='text-caribbeangreen-300 text-sm'>Years of Experience</p>
+                <div className='flex flex-row gap-5 items-center border-r border-caribbeangreen-300 px-7 '>
+                    <p className='text-3xl font-bold animate-bounce '>10</p>
+                    <p className='text-caribbeangreen-300 text-sm animate-bounce'>Years of Experience</p>
                 </div>
 
                 <div className='flex gap-5 items-center px-7'>
-                <p className='text-3xl font-bold'>250</p>
-                    <p className='text-caribbeangreen-300 text-sm'>TYpe of Courses</p>
+                <p className='text-3xl font-bold animate-bounce'>250</p>
+                    <p className='text-caribbeangreen-300 text-sm animate-bounce'>TYpe of Courses</p>
                 </div>
 
             </div>
